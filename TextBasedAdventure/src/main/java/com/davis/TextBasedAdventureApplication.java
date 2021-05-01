84c2079a67ee877021d83637590c266ec8905710
kennethdavis391 <kennethdavis391@gmail.com>
2021-05-01 08:14:53 -0400

    Base spring project created

---
+++ TextBasedAdventure/src/main/java/com/davis/TextBasedAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.davis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextBasedAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextBasedAdventureApplication.class, args);
+	}
+
+}
